4261ec81c7b02f8a6fed9507c32a56d8168943ba
Yerosan Bekele <lebrondee20@gmail.com>
2026-07-03 08:25:13 +0300

    lexer done

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/yer0san/glox
 
 go 1.26.4
+
+tool golang.org/x/tools/cmd/stringer
+
+require (
+	golang.org/x/mod v0.37.0 // indirect
+	golang.org/x/sync v0.21.0 // indirect
+	golang.org/x/tools v0.47.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+)
+
+type Lox struct {
+}
+
+var hadError bool = false
+
+// he did a different design on the l := Lox{}, keep that in mind if this breaks
+// vm := Lox{} --global, idek what that means yet :)
+
+func main(){
+	l := Lox{}
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: glox [script]");
+		os.Exit(64)
+	}
+	if len(os.Args) == 2 {
+		l.runFile(os.Args[1])
+	} else {
+		l.runPrompt();
+	}
+}
+
+func (l *Lox) runFile(path string) error {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	l.run(string(bytes))
+	if hadError {
+		os.Exit(65)
+	}
+	return nil
+}
+
+func (l *Lox) runPrompt() error {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		if ok := scanner.Scan(); !ok{
+			break
+		}
+		line := scanner.Text()
+
+		l.run(line)
+		hadError = false
+	}
+	return scanner.Err()
+}
+
+func (l *Lox) run(source string) {
+	lexer := NewLexer(source)
+
+	tokens := lexer.scanTokens()
+
+	for _, token := range tokens {
+		fmt.Println(token)
+	}
+}
+
+func reportError(line int, message string) {
+	report(line, "", message)
+}
+
+func report(line int, where string, message string) {
+	fmt.Printf("[line %d Error %s: %s\n", line, where, message)
+	hadError = true;
+}
+
+
